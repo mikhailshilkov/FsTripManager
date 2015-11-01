ab33411e79c90a95ab7113770ff5c679ef1f9827
kiwo <shilkov@gmail.com>
2015-11-01 22:20:34 +0100

    Very simple calculation of current point in a planned trip path

--- FsTripManager/ETA.fs
+++ FsTripManager/ETA.fs
@@ -1,8 +1,13 @@
 ﻿module ETA
 
+[<Measure>] 
+type degree
+
+type Geo = float<degree>
+
 type Location =
-  {Latitude: double;
-   Longitude: double;}
+  {Latitude: Geo;
+   Longitude: Geo;}
 
 type Position =
   {ModemID: string;
@@ -20,6 +25,10 @@ type Movement =
    STD: System.DateTime;
    STA: System.DateTime;}
 
+type CheckPoint =
+  {Location: Location;
+   RemainingTime: System.TimeSpan}
+
 let isPositionInMovement movement position =
   let doesModemBelongToMovement movement modemID =
     movement.Vehicles |> List.exists (fun v -> v.ModemID = modemID)
@@ -29,14 +38,26 @@ let isPositionInMovement movement position =
     doesModemBelongToMovement movement position.ModemID
 
 let distanceTo location1 location2 =
-  sqrt((location1.Latitude - location2.Latitude) ** 2.0 + (location1.Longitude - location2.Longitude) ** 2.0)
+  sqrt((location1.Latitude - location2.Latitude) * (location1.Latitude - location2.Latitude) + (location1.Longitude - location2.Longitude) * (location1.Longitude - location2.Longitude))
 
 let isAtProximity location1 location2 =
-  distanceTo location1 location2 < 0.1
+  distanceTo location1 location2 < 0.1<degree>
 
 let isAtDestination movement position =
   isAtProximity position.Location movement.Destination
 
 let isAtOrigin movement position =
-  isAtProximity position.Location movement.Destination
+  isAtProximity position.Location movement.Origin
 
+let makeStraightPath origin destination (duration: System.TimeSpan) =
+  let deltas = [destination.Latitude - origin.Latitude; destination.Longitude - origin.Longitude]
+  let legCount = (List.max deltas) / 0.01<degree>
+  let steps = deltas |> List.map (fun d -> d / legCount)
+  seq [1.0..legCount] 
+    |> Seq.map (fun n -> 
+                { Location = { Latitude = origin.Latitude + n * steps.[0]; Longitude = origin.Longitude + n * steps.[1] }; 
+                  RemainingTime = System.TimeSpan.FromTicks(int64 ((float duration.Ticks) * (1.0 - (n / legCount)))) }
+               )
+
+let findNearestPoint path location =
+  path |> Seq.minBy (fun p -> distanceTo location p.Location)
--- FsTripManager/Program.fs
+++ FsTripManager/Program.fs
@@ -1,7 +1,17 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open ETA
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+
+    let origin = { Latitude = 55.0<degree>; Longitude = 5.0<degree>}
+    let destination = { Latitude = 56.0<degree>; Longitude = 4.0<degree>}
+    let current = { Latitude = 55.27<degree>; Longitude = 4.66<degree>}
+    let vehicle = { ModemID = "Modem1"; LicensePlate = "01-ABC-2"}
+    let movement = { Vehicles = [vehicle]; Origin = origin; Destination = destination; STD = System.DateTime.Now; STA = System.DateTime.Now.AddHours(2.0) }
+    let position =  { ModemID = "Modem1"; Location = origin; Timestamp = System.DateTime.Now }
+    let path = makeStraightPath origin destination (System.TimeSpan.FromMinutes(100.0))
+    let currentCheckpoint = findNearestPoint path current
+
+    printfn "%A" currentCheckpoint
+    System.Console.ReadKey() |> ignore
     0 // return an integer exit code
